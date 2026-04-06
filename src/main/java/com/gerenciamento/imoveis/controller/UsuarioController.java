402294bc7abf27893afa36615781d05f0c8938b9
leandro.alcantara <leandro.alcantara14@gmail.com>
2026-04-06 19:53:40 -0300

    Ajuste API de usuários

--- src/main/java/com/gerenciamento/imoveis/controller/UsuarioController.java
+++ src/main/java/com/gerenciamento/imoveis/controller/UsuarioController.java
@@ -6,12 +6,14 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gerenciamento.imoveis.dto.AtualizarUsuarioDTO;
+import com.gerenciamento.imoveis.dto.CriarUsuarioDTO;
 import com.gerenciamento.imoveis.dto.UsuarioDTO;
 import com.gerenciamento.imoveis.service.UsuarioService;
 
@@ -23,6 +25,11 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioController {
 
     private final UsuarioService service;
+
+    @PostMapping
+    public UsuarioDTO criar(@RequestBody CriarUsuarioDTO dto) {
+        return service.criar(dto);
+    }
 
     @GetMapping
     public List<UsuarioDTO> listar() {
