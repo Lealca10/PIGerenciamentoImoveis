5444485f5a8dc7ab31e825071a056086ffaca321
Leandro de Alcantara <leandro.alcantara14@gmail.com>
2026-04-21 20:13:02 -0300

    Testes, Retorno usuario, CORS

--- src/main/java/com/gerenciamento/imoveis/config/SecurityConfig.java
+++ src/main/java/com/gerenciamento/imoveis/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().permitAll() // 👈 TEMPORÁRIO PRA TESTE
--- src/main/java/com/gerenciamento/imoveis/dto/UsuarioDTO.java
+++ src/main/java/com/gerenciamento/imoveis/dto/UsuarioDTO.java
@@ -2,6 +2,7 @@ package com.gerenciamento.imoveis.dto;
 
 import java.util.UUID;
 
+import com.gerenciamento.imoveis.entity.TipoUsuario;
 import com.gerenciamento.imoveis.entity.Usuario;
 
 import lombok.Data;
@@ -12,10 +13,12 @@ public class UsuarioDTO {
     private UUID id;
     private String nome;
     private String email;
+    private TipoUsuario tipoUsuario;
 
     public UsuarioDTO(Usuario usuario) {
         this.id = usuario.getId();
         this.nome = usuario.getNome();
         this.email = usuario.getEmail();
+        this.tipoUsuario = usuario.getTipoUsuario();
     }
 }
